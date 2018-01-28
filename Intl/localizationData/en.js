88a984a83dc6ab754f8414b0eb04fc2d2791394e
Jason Law <jaylaw81@gmail.com>
2018-01-27 22:03:14 -0500

    first commit

---
+++ Intl/localizationData/en.js
@@ -0,0 +1,16 @@
+export default {
+  locale: 'en',
+  messages: {
+    headerLinkMain: 'Main',
+    headerLinkTodo: 'Todos',
+    siteTitle: 'Vectorform TodoList Challenge',
+    addTodo: 'Add todo',
+    addTodoPlaceholder: 'Enter todo name',
+    deleteCompletedTodos: 'Clear completed todos',
+    switchLanguage: 'Switch Language:',
+    footerText: '',
+    welcomePageTitle: 'Welcome',
+    welcomePageText: 'This todo list uses MERN.io',
+    todoToggleStatus: 'Toggle status',
+  },
+};
---
+++ README.md
@@ -0,0 +1,12 @@
+# Vectorform Code Challenge - TodoList
+
+## Usage
+
+```
+  npm install
+  npm start
+```
+
+### Disclaimer!
+
+Webpack runs a bit slow, so give it some time to load!!!
